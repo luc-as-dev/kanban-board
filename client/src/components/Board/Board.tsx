a7925738b1421791bd2674004ea1634262cc08b6
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-23 13:01:21 +0100

    added drag and drop

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -1,25 +1,63 @@
 import React from "react";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 import { useBoardsContext } from "../../context/BoardsContext";
 import Ticket from "../Ticket/Ticket";
 import "./Board.scss";
 
 export default function Board() {
   const { currentProject } = useBoardsContext();
+
+  function onDragEnd(result: DropResult) {
+    if (!result.destination) return;
+
+    const { source, destination } = result;
+
+    const board = [...currentProject.board];
+    const sourceIndex = board.findIndex(
+      (column) => column.name === source.droppableId
+    );
+    const destinationIndex = board.findIndex(
+      (column) => column.name === destination.droppableId
+    );
+    const [removedTicket] = board[sourceIndex].tickets.splice(source.index, 1);
+
+    board[destinationIndex].tickets.splice(destination.index, 0, removedTicket);
+  }
+
   return (
     <div className="board">
-      {currentProject.board.map((list) => (
-        <div key={list.name} className="board-column">
-          <div className="board-column-header">
-            <div
-              className={`board-column-header-icon ${list.name.toLowerCase()}`}
-            ></div>
-            <h4 className="board-column-header-label">{list.name}</h4>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {currentProject.board.map((list) => (
+          <div key={list.name}>
+            <Droppable droppableId={list.name}>
+              {(provided, snapshot) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  key={list.name}
+                  className="board-column"
+                >
+                  <div className="board-column-header">
+                    <div
+                      className={`board-column-header-icon ${list.name.toLowerCase()}`}
+                    ></div>
+                    <h4 className="board-column-header-label">{list.name}</h4>
+                  </div>
+                  {list.tickets.length > 0 ? (
+                    list.tickets.map((ticket, index) => (
+                      <Ticket ticket={ticket} index={index} />
+                    ))
+                  ) : (
+                    <div className="board-column-placeholder">
+                      <p>Add ticket to the list by dropping it here</p>
+                    </div>
+                  )}
+                </div>
+              )}
+            </Droppable>
           </div>
-          {list.tickets.map((ticket) => (
-            <Ticket key={ticket.id} ticket={ticket} />
-          ))}
-        </div>
-      ))}
+        ))}
+      </DragDropContext>
     </div>
   );
 }
--- client/src/components/Ticket/Ticket.tsx
+++ client/src/components/Ticket/Ticket.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+import {
+  Draggable,
+  DraggableProvided,
+  DraggableStateSnapshot,
+} from "react-beautiful-dnd";
 import { ITicket } from "../../context/projects-data";
 import "./Ticket.scss";
 
 type Props = {
   ticket: ITicket;
+  index: number;
 };
 
-export default function Ticket({ ticket }: Props) {
+export default function Ticket({ ticket, index }: Props) {
   return (
-    <div className="ticket">
-      <h3>{ticket.title}</h3>
-      <p>{ticket.description}</p>
-      <p>{ticket.subtasks.length} Subtasks</p>
-    </div>
+    <>
+      <Draggable key={ticket.id} draggableId={ticket.id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            className="ticket"
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <h3>{ticket.title}</h3>
+            <p>{ticket.description}</p>
+            <p>{ticket.subtasks.length} Subtasks</p>
+          </div>
+        )}
+      </Draggable>
+    </>
   );
 }
