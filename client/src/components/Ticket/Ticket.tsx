97bcb19b9af1c17117c3039754f0663c9dec9516
Lucas Karlsson <lucas.karlsson@post.com>
2023-04-04 15:56:26 +0200

    showing subtask-list when clicking on task

--- client/src/components/Ticket/Ticket.tsx
+++ client/src/components/Ticket/Ticket.tsx
@@ -1,11 +1,7 @@
-import React from "react";
-import {
-  Draggable,
-  DraggableProvided,
-  DraggableStateSnapshot,
-} from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
 import { ITicket } from "../../context/projects-data";
 import "./Ticket.scss";
+import { useState } from "react";
 
 type Props = {
   ticket: ITicket;
@@ -13,18 +9,30 @@ type Props = {
 };
 
 export default function Ticket({ ticket, index }: Props) {
+  const [showSubtasks, setShowSubtasks] = useState<boolean>(false);
+
   return (
     <Draggable key={ticket.id} draggableId={ticket.id} index={index}>
-      {(provided, snapshot) => (
+      {(provided) => (
         <div
           className="ticket"
           ref={provided.innerRef}
+          onClick={() => {
+            setShowSubtasks((s) => !s);
+          }}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
           <h3>{ticket.title}</h3>
           <p>{ticket.description}</p>
           <p>{ticket.subtasks.length} Subtasks</p>
+          {showSubtasks && ticket.subtasks.length !== 0 && (
+            <ul>
+              {ticket.subtasks.map((subtask, index) => (
+                <li key={index}>{subtask}</li>
+              ))}
+            </ul>
+          )}
         </div>
       )}
     </Draggable>
